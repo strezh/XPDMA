4ecfb823ef1429dadb3398e07d0cf40a9e045507
strezhik <strezhik@gmail.com>
2015-08-05 15:41:46 +0300

    add driver kernel registration

--- driver/xpdma_driver.c
+++ driver/xpdma_driver.c
@@ -6,12 +6,23 @@
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 #include <linux/init.h>		/* Needed for the macros */
 #include <linux/fs.h>       /* Needed for files operations */
+#include <linux/pci.h>      /* Needed for PCI */
 
 #include "xpdma_driver.h"
 
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_DESCRIPTION("PCIe driver for Xilinx CDMA subsystem (XAPP1171), Linux");
 MODULE_AUTHOR("Strezhik Iurii");
+
+#define HAVE_KERNEL_REG   0x01      // Kernel registration
+
+int gDrvrMajor = 241;               // Major number not dynamic.
+struct pci_dev *gDev = NULL;        // PCI device structure.
+unsigned int gStatFlags = 0x00;     // Status flags used for cleanup
+unsigned long gBaseHdwr;            // Base register address (Hardware address)
+unsigned long gBaseLen;             // Base register address Length
+
+
 
 // Prototypes
 ssize_t xpdma_write (struct file *filp, const char *buf, size_t count, loff_t *f_pos);
@@ -32,39 +43,76 @@ struct file_operations xpdma_intf = {
 
 ssize_t xpdma_write (struct file *filp, const char *buf, size_t count, loff_t *f_pos)
 {
-    return 0;
+    return (SUCCESS);
 }
 
 ssize_t xpdma_read (struct file *filp, char *buf, size_t count, loff_t *f_pos)
 {
-    return 0;
+    return (SUCCESS);
 }
 
 int xpdma_ioctl (struct file *filp, unsigned int cmd, unsigned long arg)
 {
-    return 0;
+    return (SUCCESS);
 }
 
 int xpdma_open(struct inode *inode, struct file *filp)
 {
     printk(KERN_INFO"%s: Open: module opened\n", DEVICE_NAME);
-    return 0;
+    return (SUCCESS);
 }
 
 int xpdma_release(struct inode *inode, struct file *filp)
 {
     printk(KERN_INFO"%s: Release: module released\n", DEVICE_NAME);
-    return 0;
+    return (SUCCESS);
 }
 
 static int xpdma_init (void)
 {
+    gDev = pci_get_device(VENDOR_ID, DEVICE_ID, gDev);
+    if (NULL == gDev) {
+        printk(KERN_WARNING"%s: Init: Hardware not found.\n", DEVICE_NAME);
+        return (CRIT_ERR);
+    }
 
+    // Get Base Address of BAR0 registers
+    gBaseHdwr = pci_resource_start(gDev, 0);
+    if (0 > gBaseHdwr) {
+        printk(KERN_WARNING"%s: Init: Base Address not set.\n", DEVICE_NAME);
+        return (CRIT_ERR);
+    }
+
+    printk(KERN_INFO"%s: Init: Base hw val %X\n", DEVICE_NAME, (unsigned int) gBaseHdwr);
+
+    // Get the Base Address Length
+    gBaseLen = pci_resource_len(gDev, 0);
+
+    // Print the Base Address Length to Kernel Log
+    printk(KERN_INFO"%s: Init: Base hw len %d\n", DEVICE_NAME, (unsigned int) gBaseLen);
+
+    // Register driver as a character device.
+    if (0 > register_chrdev(gDrvrMajor, DEVICE_NAME, &xpdma_intf)) {
+        printk(KERN_WARNING"%s: Init: will not register\n", DEVICE_NAME);
+        return (CRIT_ERR);
+    }
+    printk(KERN_INFO"%s: Init: module registered\n", DEVICE_NAME);
+
+    gStatFlags = gStatFlags | HAVE_KERNEL_REG;
+    printk("%s driver is loaded\n", DEVICE_NAME);
+
+    return 0;
 }
 
 static void xpdma_exit (void)
 {
+    // Unregister Device Driver
+    if (gStatFlags & HAVE_KERNEL_REG) {
+        unregister_chrdev(gDrvrMajor, DEVICE_NAME);
+    }
 
+    gStatFlags = 0;
+    printk(KERN_ALERT"%s driver is unloaded\n", DEVICE_NAME);
 }
 
 module_init(xpdma_init);
--- driver/xpdma_driver.h
+++ driver/xpdma_driver.h
@@ -3,4 +3,10 @@
 
 #define DEVICE_NAME "xpdma"
 
+#define VENDOR_ID 0x10EE // Xilinx Vendor ID
+#define DEVICE_ID 0x7024
+
+#define SUCCESS      0
+#define CRIT_ERR    -1
+
 #endif //XPDMA_DRIVER_H
